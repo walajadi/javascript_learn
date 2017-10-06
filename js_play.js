5919d5c743bdc99ac1a06583f8280e1743bae243
slim <=>
2017-10-06 17:27:01 +0200

    callback functions

--- js_play.js
+++ js_play.js
@@ -8,3 +8,23 @@ var global = 1;
 
 f();
 f();
+
+// callback functions
+
+function one() {
+     return 1;
+ }
+ 
+function two() {
+	return 2;
+}
+
+function invoke_and_add(a, b){
+     return a() + b();
+}
+invoke_and_add(one, two); // return 3
+
+// 2nd option
+invoke_and_add(function(){return 1;}, function(){return 2;})
+
+
