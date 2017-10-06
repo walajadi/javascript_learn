a5a86c497b9999dd3a0ababc11af2f114e449a1d
slim <=>
2017-10-06 18:08:16 +0200

    create objects

---
+++ objects.js
@@ -0,0 +1,19 @@
+// alert('objects.js');
+
+// simple obj
+var hero = {
+	breed: 'Turtle',
+    occupation: 'Ninja',
+
+};
+
+// obj contains an obj
+var book = {
+  name: 'Catch-22',
+  published: 1961,
+  author: {
+    firstname: 'Joseph',
+    lastname: 'Heller'
+  }
+};
+
