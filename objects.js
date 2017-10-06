4131e6bffb1b21f2333ffc14b7d2d9a9c97b3ab7
slim <=>
2017-10-06 18:22:07 +0200

    objects with constructors

--- objects.js
+++ objects.js
@@ -4,8 +4,11 @@
 var hero = {
 	breed: 'Turtle',
     occupation: 'Ninja',
-
+    say: function() {
+       return 'I am ' + hero.occupation;
+     }
 };
+
 
 // obj contains an obj
 var book = {
@@ -17,3 +20,28 @@ var book = {
   }
 };
 
+// some methods
+typeof hero.breed;
+hero.name = 'leonardo';
+delete hero.name; // return True.
+
+// using init constructor
+function Hero() {
+     this.occupation = 'Ninja';
+}
+
+var hero = new Hero();
+hero.occupation;
+// "Ninja"
+
+// with function. 
+function Hero(name) {
+     this.name = name;
+     this.occupation = 'Ninja';
+     this.whoAreYou = function() {
+       return "I'm " + this.name + " and I'm a " + this.occupation;
+     }
+}
+var h1 = new Hero('Michelangelo');
+h2 = new Hero('Donatello');
+h1.whoAreYou();
