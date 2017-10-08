6e5610a6f2ebfcffc824e7fd5c8d319426e380ca
slim <=>
2017-10-08 14:58:26 +0200

    use prototypes to add methods and properties

--- proto_type.js
+++ proto_type.js
@@ -1,1 +1,27 @@
 console.log('proto type');
+
+// functions : access properties 
+
+function foo(a, b){
+	return a * b;
+};
+// foo.length => 2
+// foo.constructor => Functions
+
+// Adding Methods and Properties Using the Prototype
+function Gadget(name, color) {
+     this.name = name;
+     this.color = color;
+     this.whatAreYou = function(){
+       return 'I am a ' + this.color + ' ' + this.name;
+    }
+};
+
+Gadget.prototype.price = 100;
+Gadget.prototype.rating = 3;
+Gadget.prototype.getInfo = function() {
+	return 'Rating: ' + this.rating + ', price: ' + this.price;
+};
+
+
+
