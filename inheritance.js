c2cce0e3dbe55db453ee0b0ba6c82e33c23d5d33
slim <=>
2017-10-08 16:58:19 +0200

    moving shared properties to prototype properties

--- inheritance.js
+++ inheritance.js
@@ -37,3 +37,29 @@ Shape.prototype.isPrototypeOf(my); // True
 TwoDShape.prototype.isPrototypeOf(my); // True
 Triangle.prototype.isPrototypeOf(my); // True
 
+// Moving Shared Properties to the Prototype
+
+function Shape(){}
+// augment prototype
+Shape.prototype.name = 'shape';
+Shape.prototype.toString = function() {return this.name;};
+function TwoDShape(){}
+
+// take care of inheritance
+TwoDShape.prototype = new Shape();
+TwoDShape.prototype.constructor = TwoDShape;
+// augment prototype
+TwoDShape.prototype.name = '2D shape';
+
+
+function Triangle(side, height) {
+	this.side = side;
+	this.height = height;
+}
+// take care of inheritance
+Triangle.prototype = new TwoDShape();
+Triangle.prototype.constructor = Triangle;
+// augment prototype
+Triangle.prototype.name = 'Triangle';
+Triangle.prototype.getArea = function(){return this.side * this.height / 2;};
+
